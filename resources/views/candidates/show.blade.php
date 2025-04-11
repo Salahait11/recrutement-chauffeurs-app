1bf95986cf9366ddff3e1e6d1a54ee293702a2f5
aithammousalah0 <aithammousalah0@gmail.com>
2025-04-11 21:08:09 +0000

    d

--- resources/views/candidates/show.blade.php
+++ resources/views/candidates/show.blade.php
@@ -68,7 +68,7 @@
                         <div class="md:col-span-1 font-semibold">{{ __('Statut') }}</div>
                         <div class="md:col-span-2 flex items-center space-x-4">
                             <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full
-                                @if($candidate->status === Candidate::STATUS_EMBAUCHE)
+                                @if($candidate->status === \App\Models\Candidate::STATUS_EMBAUCHE)
                                     bg-green-100 text-green-800 dark:bg-green-800 dark:text-green-100
                                 @elseif($candidate->status === Candidate::STATUS_REFUSE)
                                     bg-red-100 text-red-800 dark:bg-red-800 dark:text-red-100
@@ -76,12 +76,12 @@
                                     bg-blue-100 text-blue-800 dark:bg-blue-800 dark:text-blue-100
                                 @endif
                             ">
-                                {{ Candidate::$statuses[$candidate->status] }}
+                                {{ $statuses[$candidate->status] }}
                             </span>
                             
                             <form method="POST" action="{{ route('candidates.update', $candidate->id) }}">
                                 @csrf
-                                @method('PUT')
+                                @method('PUT') 
                                 <select name="status" onchange="this.form.submit()" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                     <option disabled>{{ __('Changer le statut') }}</option>
                                     @foreach(Candidate::$statuses as $value => $label)
